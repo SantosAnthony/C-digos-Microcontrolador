536b20b7962982612ecb7522d78313901d453bbd
Anthony Silva Santos <126997184+SantosAnthony@users.noreply.github.com>
2026-01-28 11:36:38 -0300

    Add files via upload

---
+++ Segunda Semana/Código_de_interepretador_arduino/main.c
@@ -0,0 +1,103 @@
+#include <SPI.h>
+#include <SD.h>
+#include <RTClib.h>
+
+const int pinoCS = 4;
+const int PinoRele = 7;
+
+RTC_DS1307 rtc;
+File arquivo;
+
+unsigned long ultimoCheck = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PinoRele, OUTPUT);
+  digitalWrite(PinoRele, LOW);
+
+  if (!rtc.begin()) {
+    while (1);
+  }
+
+  if (!rtc.isrunning()) {
+    rtc.adjust(DateTime(F(_DATE), F(TIME_)));
+  }
+
+  if (!SD.begin(pinoCS)) {
+    while (1);
+  }
+}
+
+void loop() {
+    verificarAgenda();
+    delay(60000);
+}
+
+void verificarAgenda() {
+  DateTime agora = rtc.now();
+  
+  char dataHoje[11]; 
+  sprintf(dataHoje, "%02d/%02d/%04d", agora.day(), agora.month(), agora.year());
+  
+  char horaAgora[6];
+  sprintf(horaAgora, "%02d:%02d", agora.hour(), agora.minute());
+
+  arquivo = SD.open("dados.csv");
+  if (!arquivo) {
+    Serial.println("Erro ao abrir dados.csv");
+    return;
+  }
+
+  char bufferLinha[100];
+  bool encontrouHoje = false;
+
+  while (arquivo.available()) {
+    int len = arquivo.readBytesUntil('\n', bufferLinha, 99);
+    bufferLinha[len] = '\0';
+
+    char* token = strtok(bufferLinha, ";");
+    
+
+    if (!token) continue; 
+
+    if (strcmp(token, dataHoje) == 0) {
+      Serial.print("Agenda de hoje encontrada: ");
+      Serial.println(dataHoje);
+      encontrouHoje = true;
+
+      for (int i = 1; i <= 4; i++) {
+        char* horaInicio = strtok(NULL, ";");
+        char* duracaoStr = strtok(NULL, ";");
+
+        if (horaInicio && duracaoStr) {
+          if (strcmp(horaInicio, horaAgora) == 0) {
+            int tempoRega = atoi(duracaoStr);
+            if (tempoRega > 0) {
+              Serial.print("HORA DE REGAR! Inicio: ");
+              Serial.print(horaInicio);
+              Serial.print(" por ");
+              Serial.print(tempoRega);
+              Serial.println(" minutos.");
+              
+              arquivo.close(); 
+              
+              executarRega(tempoRega);
+              return;
+            }
+          }
+        }
+      }
+      break; 
+    }
+  }
+  arquivo.close();
+}
+
+void executarRega(int minutos) {
+  digitalWrite(PinoRele, HIGH);
+  unsigned long tempoMs = minutos * 60000UL;
+  
+  delay(tempoMs);
+  
+  digitalWrite(PinoRele, LOW);
+}
