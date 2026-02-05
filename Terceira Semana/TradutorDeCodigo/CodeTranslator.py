227d881a7adf7b810c1ae689876ac88cc34a5d17
Anthony Silva Santos <126997184+SantosAnthony@users.noreply.github.com>
2026-02-05 11:00:53 -0300

    Add files via upload

--- Terceira Semana/TradutorDeCodigo/CodeTranslator.py
+++ Terceira Semana/TradutorDeCodigo/CodeTranslator.py
@@ -1,3 +1,4 @@
+import datetime
 import csv
 archiveName = 'Irrigation_Plan.txt'
 filtredArchiveName = 'Data_Archive.txt'
@@ -8,16 +9,16 @@ with open(archiveName, 'r') as f:
     for line in f:
         content = line.split("#")[0].strip().rstrip("|")
         if content != '' and content != '\n':
-            filtredLines.append(content)
+            contentInfo = content.split(" | ")
+            date = contentInfo[0].split("-")
+
+            newDate = "-".join(reversed(date))
+            contentInfo[0] = newDate
             
-
-
-reFiltredLines = []
-for line in filtredLines:
-    reFiltredLines.append(line.replace("-","/").replace("; ",",").replace(" | ",","))
-
+            stringInfo = ",".join(contentInfo).replace(" ","").replace("-","/").replace(";",",").replace(" | ",",")
+            filtredLines.append(stringInfo)
 
 with open(filtredArchiveName, 'w', newline='') as f:
     writer = csv.writer(f)
-    for line in reFiltredLines:
+    for line in filtredLines:
         writer.writerow(line.split(","))
